4167d260e2c5c781cb856a40ed8982d2cd126a61
Neeraj Sharma <nitsnwits@gmail.com>
2014-10-24 23:04:20 -0700

    hibernate checkin

--- src/amazon/hibernate/app/Test.java
+++ src/amazon/hibernate/app/Test.java
@@ -22,8 +22,8 @@ public class Test {
 
 	int count = 0;
 	Session session;
-	Transaction tx; 
-	ArrayList<DBTable> dbArrayList = new ArrayList<>();
+	Transaction tx;
+	ArrayList<DBTable> dbArrayList = new ArrayList<DBTable>();
 
 	/**
 	 * @param args
@@ -35,46 +35,47 @@ public class Test {
 		try {
 			Test test = new Test();
 			GenerateReview generateReview = new GenerateReview();
-			fileStream = new FileInputStream("/home/abhinav/Downloads/Arts.txt.gz");
+			fileStream = new FileInputStream(
+					"/Users/neerajsharma/Downloads/amazonData/Arts.txt.gz");
 			InputStream gzipStream = new GZIPInputStream(fileStream);
 			Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
 			BufferedReader buffered = new BufferedReader(decoder);
 
 			long date1 = System.currentTimeMillis();
-			System.out.println("Start time: "+ date1);
+			System.out.println("Start time: " + date1);
 			String line = null;
-			HashMap<String, String> reviews = new HashMap<>();
+			HashMap<String, String> reviews = new HashMap<String, String>();
 
-			while((line=buffered.readLine())!=null){
+			while ((line = buffered.readLine()) != null) {
 
-				if(line.length()!=0){
+				if (line.length() != 0) {
 
-					if(line.startsWith("product")){
+					if (line.startsWith("product")) {
 
 						String[] string = line.split(":");
 						String[] string1 = string[0].split("/");
 						reviews.put(string1[1], string[1].trim());
-					}				
-				}else{
+					}
+				} else {
 
-					try {				
+					try {
 						DBTable dbTable = generateReview.createReview(reviews);
 						test.dbArrayList.add(dbTable);
-						if(test.dbArrayList.size()==1000){
+						if (test.dbArrayList.size() == 1000) {
 							test.commitMethod(test.dbArrayList);
-							test.dbArrayList = new ArrayList<>();
+							test.dbArrayList = new ArrayList<DBTable>();
 						}
 
 					} finally {
-						//session.close();
+						// session.close();
 					}
-					reviews = new HashMap<>();
+					reviews = new HashMap<String, String>();
 				}
 			}
 			long date2 = System.currentTimeMillis();
-			System.out.println("End time: "+ date2);
-			System.out.println((date2-date1)+"  milli seconds");
-			System.out.println((date2-date1)/1000+"  seconds");
+			System.out.println("End time: " + date2);
+			System.out.println((date2 - date1) + "  milli seconds");
+			System.out.println((date2 - date1) / 1000 + "  seconds");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -101,33 +102,23 @@ public class Test {
 
 }
 
-/*if(string1[1].equals("productId")){
-reviews.put("productId", string[1].trim());
-}else if(string1[1].equals("title")){
-reviews.put("title", string[1].trim());
-}else if(string1[1].equals("price")){
-reviews.put("price", string[1].trim());
-}
-}else if(line.startsWith("review")){
-
-String[] string = line.split(":");
-String[] string1 = string[0].split("/");
-
-
-//if(string1[1].equals("userId")){
-//reviews.put("userId", string[1].trim());
-//}else if(string1[1].equals("profileName")){
-//reviews.put("profileName", string[1].trim());
-//}else if(string1[1].equals("helpfulness")){
-//reviews.put("helpfulness", string[1].trim());
-//}else if(string1[1].equals("score")){
-//reviews.put("score", string[1].trim());
-//}else if(string1[1].equals("time")){
-//reviews.put("time", string[1].trim());
-//}else if(string1[1].equals("summary")){
-//reviews.put("summary", string[1].trim());
-//}else if(string1[1].equals("text")){
-//reviews.put("text", string[1].trim());
-//}
- */					
+/*
+ * if(string1[1].equals("productId")){ reviews.put("productId",
+ * string[1].trim()); }else if(string1[1].equals("title")){ reviews.put("title",
+ * string[1].trim()); }else if(string1[1].equals("price")){ reviews.put("price",
+ * string[1].trim()); } }else if(line.startsWith("review")){
+ * 
+ * String[] string = line.split(":"); String[] string1 = string[0].split("/");
+ * 
+ * 
+ * //if(string1[1].equals("userId")){ //reviews.put("userId", string[1].trim());
+ * //}else if(string1[1].equals("profileName")){ //reviews.put("profileName",
+ * string[1].trim()); //}else if(string1[1].equals("helpfulness")){
+ * //reviews.put("helpfulness", string[1].trim()); //}else
+ * if(string1[1].equals("score")){ //reviews.put("score", string[1].trim());
+ * //}else if(string1[1].equals("time")){ //reviews.put("time",
+ * string[1].trim()); //}else if(string1[1].equals("summary")){
+ * //reviews.put("summary", string[1].trim()); //}else
+ * if(string1[1].equals("text")){ //reviews.put("text", string[1].trim()); //}
+ */
 
